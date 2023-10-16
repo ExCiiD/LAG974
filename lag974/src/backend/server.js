a6c23cf442cba45aed4534115b448a45989c6082
ExCiiD <132538296+ExCiiD@users.noreply.github.com>
2023-10-16 17:58:07 +0400

    routes for jeu and evenements

--- lag974/src/backend/server.js
+++ lag974/src/backend/server.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 //routes
-/* import {  } from './routes/..Router.js'; */
+import { jeuRoute } from './routes/jeuRoutes.js';
+import { evenementRoute } from './routes/evenementRoutes.js'; 
 
 //connection a la base de donnés
-/* import { connectDB } from './config/mongodb.js';
+import { connectDB } from './db/mongodb.js';
 dotenv.config();
-connectDB(); */
+connectDB(); 
 
 const port = 7000;
 const app = express();
 
-/* app.use(express.json()); */
+app.use(express.json());
+app.use(jeuRoute, evenementRoute);
 app.get('/', (req, res) => { res.send("API lag974") });
 
 
